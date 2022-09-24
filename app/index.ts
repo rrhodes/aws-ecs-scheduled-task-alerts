48754de25375cefee923b4100e1e78b61ede296a
Ross Rhodes <trh@trustpilot.com>
2022-09-24 13:22:30 +0100

    randomise success or failure within application

--- app/index.ts
+++ app/index.ts
@@ -1,5 +1,15 @@
+const getRandomBoolean = (): boolean => Math.floor(Math.random() * 2) == 1;
+
 const main = () => {
-  console.log('I\'m alive!')
+  const randomBoolean = getRandomBoolean();
+
+  if (randomBoolean) {
+    console.log('I\'m alive!')
+  } else {
+    const message = `Send help!`;
+    console.error(message)
+    throw Error(message);
+  }
 };
 
 main();
