0d24995f176be615534caf42fa6391e453eccafc
Ross Rhodes <trh@trustpilot.com>
2022-09-24 11:39:11 +0100

    enable container insights for ecs cluster

--- lib/aws-ecs-triggered-task-alerts-stack.ts
+++ lib/aws-ecs-triggered-task-alerts-stack.ts
@@ -12,6 +12,7 @@ export class AwsEcsTriggeredTaskAlertsStack extends Stack {
 
     const cluster = new Cluster(this, 'Cluster', {
       clusterName: 'fargate-cluster-example',
+      containerInsights: true,
       enableFargateCapacityProviders: true,
     });
 
